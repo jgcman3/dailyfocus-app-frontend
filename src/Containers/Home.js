f1a6ce31d13a3bb163b844dc093a98f4ad1751ed
jgcman3 <jgcman3@gmail.com>
2020-02-12 11:00:54 +0900

    add image

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { InputGroup, FormControl } from "react-bootstrap";
 import { API } from "aws-amplify";
+import Clock from "react-live-clock";
 import "./Home.css";
 
 export default function Home(props) {
@@ -34,8 +34,9 @@ export default function Home(props) {
   function renderNotAuthenticated() {
     return (
       <div className="lander">
-        <h1>_________________</h1>
-        <p>Let's focus on just one thing.</p>
+        <Clock className="clock" format={"HH:mm:ss"} ticking={true} />
+        <h2>"Lost time is never found again."</h2>
+        <p>by Benjamin Franklin</p>
       </div>
     );
   }
@@ -44,8 +45,11 @@ export default function Home(props) {
     return (
       <div className="lander">
         <h1>00:00:00</h1>
-        <p>Let's focus on just one thing.</p>
-        <h3>what is your main focus for today?</h3>
+        {/* <p>Let's focus on just one thing.</p> */}
+        <h2>what is your main focus for today?</h2>
+        <form class="js-form form">
+          <input type="text" placeholder="What is your name?" />
+        </form>
         <h4>_________________________________</h4>
         {/* Your mission is .. */}
       </div>
@@ -54,7 +58,9 @@ export default function Home(props) {
 
   return (
     <div className="Home container">
-      {props.isAuthenticated ? renderAuthenticated() : renderNotAuthenticated()}
+      {!props.isAuthenticated
+        ? renderAuthenticated()
+        : renderNotAuthenticated()}
     </div>
   );
 }
